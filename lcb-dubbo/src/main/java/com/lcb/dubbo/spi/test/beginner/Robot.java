5818f1995382a478e8a0030e7e73585fc4a470f9
changbao.li <changbao.li@bianlifeng.com>
2019-08-11 10:28:21 +0800

    2019年08月11日10:28:20

--- lcb-dubbo/src/main/java/com/lcb/dubbo/spi/test/beginner/Robot.java
+++ lcb-dubbo/src/main/java/com/lcb/dubbo/spi/test/beginner/Robot.java
@@ -2,6 +2,9 @@ package com.lcb.dubbo.spi.test.beginner;
 
 import org.apache.dubbo.common.extension.SPI;
 
+/**
+ * 注解@SPI里面只有一个String类型，通过使用@SPI("dubbo")标识2接口类的具体实现，注解@SPI里面的值对应了SPI配置文件中的前缀名称。
+ */
 @SPI
 public interface Robot {
     void sayHello();
