03b9785cb453eb80e1fc25c4a0ab1ecb3d968c99
changbao.li <changbao.li@bianlifeng.com>
2019-08-14 09:39:56 +0800

    2019年08月14日09:39:55

--- lcb-dubbo/src/main/java/com/lcb/dubbo/spi/test/auto/loader/RaceCarMaker.java
+++ lcb-dubbo/src/main/java/com/lcb/dubbo/spi/test/auto/loader/RaceCarMaker.java
@@ -8,7 +8,7 @@ public class RaceCarMaker implements CarMaker {
     /**
      *  当CarMake扩展点被加载时，会自动装配WheelMaker
      *
-     *  思考：当WheelMaker有多个实现时，dubbo是通过什么机制选择来选择相关的实例呢？
+     *  思考：当WheelMaker拓展点有多个实现时，dubbo是通过什么机制选择来选择相关的实例呢？
      */
     public void setWheelMaker(WheelMaker wheelMaker) {
         this.wheelMaker = wheelMaker;
