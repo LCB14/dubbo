1458b5fc8a757ca5d3df80ecdbc99aa5f9dd2483
changbao.li <changbao.li@bianlifeng.com>
2019-08-14 09:32:48 +0800

    2019年08月14日09:32:48

--- lcb-dubbo/src/main/java/com/lcb/dubbo/spi/test/auto/packet/Test.java
+++ lcb-dubbo/src/main/java/com/lcb/dubbo/spi/test/auto/packet/Test.java
@@ -6,6 +6,7 @@ import org.apache.dubbo.common.extension.ExtensionLoader;
  * @author changbao.li
  * @Description 扩展点自动包装测试
  * @Date 2019-07-09 22:05
+ * 参考：http://dubbo.apache.org/zh-cn/docs/dev/SPI.html
  */
 public class Test {
     public static void main(String[] args) {
