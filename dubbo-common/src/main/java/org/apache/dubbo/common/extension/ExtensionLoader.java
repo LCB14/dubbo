e097d674e3a848ef0af17d5765badd407a84593f
changbao.li <changbao.li@bianlifeng.com>
2019-08-17 17:48:45 +0800

    2019年08月17日17:48:44

--- dubbo-common/src/main/java/org/apache/dubbo/common/extension/ExtensionLoader.java
+++ dubbo-common/src/main/java/org/apache/dubbo/common/extension/ExtensionLoader.java
@@ -1013,6 +1013,48 @@ public class ExtensionLoader<T> {
         return cachedAdaptiveClass = createAdaptiveExtensionClass();
     }
 
+    /**
+     *  生成的代理类大概样式举例：
+     *
+     * package org.apache.dubbo.demo.provider;
+     *
+     * import org.apache.dubbo.common.extension.ExtensionLoader;
+     *
+     * public class Protocol$Adaptive implements org.apache.dubbo.rpc.Protocol {
+     *     public void destroy() {
+     *         throw new UnsupportedOperationException("The method public abstract void org.apache.dubbo.rpc.Protocol.destroy() of interface org.apache.dubbo.rpc.Protocol is not adaptive method!");
+     *     }
+     *
+     *     public int getDefaultPort() {
+     *         throw new UnsupportedOperationException("The method public abstract int org.apache.dubbo.rpc.Protocol.getDefaultPort() of interface org.apache.dubbo.rpc.Protocol is not adaptive method!");
+     *     }
+     *
+     *     public org.apache.dubbo.rpc.Exporter export(org.apache.dubbo.rpc.Invoker arg0) throws org.apache.dubbo.rpc.RpcException {
+     *         if (arg0 == null)
+     *             throw new IllegalArgumentException("org.apache.dubbo.rpc.Invoker argument == null");
+     *         if (arg0.getUrl() == null)
+     *             throw new IllegalArgumentException("org.apache.dubbo.rpc.Invoker argument getUrl() == null");
+     *         org.apache.dubbo.common.URL url = arg0.getUrl();
+     *         String extName = (url.getProtocol() == null ? "dubbo" : url.getProtocol());
+     *         if (extName == null)
+     *             throw new IllegalStateException("Failed to get extension (org.apache.dubbo.rpc.Protocol) name from url (" + url.toString() + ") use keys([protocol])");
+     *         org.apache.dubbo.rpc.Protocol extension = (org.apache.dubbo.rpc.Protocol) ExtensionLoader.getExtensionLoader(org.apache.dubbo.rpc.Protocol.class).getExtension(extName);
+     *         return extension.export(arg0);
+     *     }
+     *
+     *     public org.apache.dubbo.rpc.Invoker refer(java.lang.Class arg0, org.apache.dubbo.common.URL arg1) throws org.apache.dubbo.rpc.RpcException {
+     *         if (arg1 == null)
+     *              throw new IllegalArgumentException("url == null");
+     *         org.apache.dubbo.common.URL url = arg1;
+     *         String extName = (url.getProtocol() == null ? "dubbo" : url.getProtocol());
+     *         if (extName == null)
+     *             throw new IllegalStateException("Failed to get extension (org.apache.dubbo.rpc.Protocol) name from url (" + url.toString() + ") use keys([protocol])");
+     *         org.apache.dubbo.rpc.Protocol extension = (org.apache.dubbo.rpc.Protocol) ExtensionLoader.getExtensionLoader(org.apache.dubbo.rpc.Protocol.class).getExtension(extName);
+     *         return extension.refer(arg0, arg1);
+     *     }
+     * }
+     *
+     */
     private Class<?> createAdaptiveExtensionClass() {
         // 按规则生成自适应拓展代码
         String code = new AdaptiveClassCodeGenerator(type, cachedDefaultName).generate();
