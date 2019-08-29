29179cace7f036d4f52352a5d831765b0f86d178
changbao.li <changbao.li@bianlifeng.com>
2019-08-29 20:05:54 +0800

    2019年08月29日20:05:53

--- dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/loadbalance/LeastActiveLoadBalance.java
+++ dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/loadbalance/LeastActiveLoadBalance.java
@@ -36,22 +36,46 @@ public class LeastActiveLoadBalance extends AbstractLoadBalance {
 
     public static final String NAME = "leastactive";
 
+    /**
+     * 1、遍历 invokers 列表，寻找活跃数最小的 Invoker；
+     *
+     * 2、如果有多个 Invoker 具有相同的最小活跃数，此时记录下这些 Invoker 在 invokers 集合中的下标，
+     * 并累加它们的权重，比较它们的权重值是否相等；
+     *
+     * 3、如果只有一个 Invoker 具有最小的活跃数，此时直接返回该 Invoker 即可；
+     *
+     * 4、如果有多个 Invoker 具有最小活跃数，且它们的权重不相等，此时处理方式和 RandomLoadBalance 一致；
+     *
+     * 5、如果有多个 Invoker 具有最小活跃数，但它们的权重相等，此时随机返回一个即可；
+     */
     @Override
     protected <T> Invoker<T> doSelect(List<Invoker<T>> invokers, URL url, Invocation invocation) {
         // Number of invokers
         int length = invokers.size();
+
         // The least active value of all invokers
+        // 最小的活跃数
         int leastActive = -1;
+
         // The number of invokers having the same least active value (leastActive)
+        // 具有相同“最小活跃数”的服务者提供者（以下用 Invoker 代称）数量
         int leastCount = 0;
+
         // The index of invokers having the same least active value (leastActive)
+        // leastIndexs 用于记录具有相同“最小活跃数”的 Invoker 在 invokers 列表中的下标信息
         int[] leastIndexes = new int[length];
+
         // the weight of every invokers
         int[] weights = new int[length];
+
         // The sum of the warmup weights of all the least active invokes
         int totalWeight = 0;
+
         // The weight of the first least active invoke
+        // 第一个最小活跃数的 Invoker 权重值，用于与其他具有相同最小活跃数的 Invoker 的权重进行对比，
+        // 以检测是否“所有具有相同最小活跃数的 Invoker 的权重”均相等
         int firstWeight = 0;
+
         // Every least active invoker has the same weight value?
         boolean sameWeight = true;
 
@@ -68,15 +92,23 @@ public class LeastActiveLoadBalance extends AbstractLoadBalance {
             // If it is the first invoker or the active number of the invoker is less than the current least active number
             if (leastActive == -1 || active < leastActive) {
                 // Reset the active number of the current invoker to the least active number
+                // 最小活跃数
                 leastActive = active;
+
                 // Reset the number of least active invokers
+                // 具有相同最小活跃数服务提供者的数量
                 leastCount = 1;
+
                 // Put the first least active invoker first in leastIndexes
+                // 记录当前拥有最小活跃数服务下标值到 leastIndexs 中
                 leastIndexes[0] = i;
+
                 // Reset totalWeight
                 totalWeight = afterWarmup;
+
                 // Record the weight the first least active invoker
                 firstWeight = afterWarmup;
+
                 // Each invoke has the same weight (only one invoker here)
                 sameWeight = true;
                 // If current invoker's active value equals with leaseActive, then accumulating.
@@ -93,10 +125,12 @@ public class LeastActiveLoadBalance extends AbstractLoadBalance {
             }
         }
         // Choose an invoker from all the least active invokers
+        // 当只有一个 Invoker 具有最小活跃数，此时直接返回该 Invoker 即可
         if (leastCount == 1) {
             // If we got exactly one invoker having the least active value, return this invoker directly.
             return invokers.get(leastIndexes[0]);
         }
+        // 有多个 Invoker 具有相同的最小活跃数，但它们之间的权重不同
         if (!sameWeight && totalWeight > 0) {
             // If (not every invoker has the same weight & at least one invoker's weight>0), select randomly based on 
             // totalWeight.
